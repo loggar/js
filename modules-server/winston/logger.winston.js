781681b919d7817c77b39f924bf6ce7b35fec49b
CHARLY LEE <webnl@DT-Charly.koi.local>
2017-11-24 14:19:44 +1100

    winston daily rotate file appender

--- modules-server/winston/logger.winston.js
+++ modules-server/winston/logger.winston.js
@@ -1,22 +1,47 @@
+/**
+ * js logger with winston
+ * @author Charly Lee <charly.loggar@gmail.com>
+ * @description format style
+ * @version 0.0.1
+ */
 var winston = require('winston');
+require('winston-daily-rotate-file');
 var dateFormat = require('dateformat');
 var path = require('path');
+var env_mode = process.env.ENV || "developement";
 
-module.exports = function(filename) {
-	if(!filename) filename = 'unknown';
-	else filename = path.basename(filename, '.js');
-	
-	return new (winston.Logger) ({
-		transports: [new (winston.transports.Console)({
-			level: 'debug', /* { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 } */
-			json: false, /* true : will log out multi-line JSON objects */
-			stringify: false,
-			timestamp : function() {
-				return dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
-			},
-			formatter : function(options) {
-				return options.timestamp() + ' [' + options.level.toUpperCase() + ']' + ' [' + filename + '] ' + (options.message ? options.message : '') + (options.meta && Object.keys(options.meta).length ? JSON.stringify(options.meta) : '');
-			}
-		})]
+module.exports = function (process_nm) {
+	if (!process_nm) process_nm = 'unknown';
+	else process_nm = path.basename(process_nm, '.js');
+
+	var timestamp = function () {
+		return dateFormat(new Date(), "yyyy-mm-dd hh:MM:ss");
+	}
+
+	var formatter = function (options) {
+		return options.timestamp() + ' [' + options.level.toUpperCase()[0] + '] [' + process_nm + '] ' + (options.message ? options.message : '') + (options.meta && Object.keys(options.meta).length ? JSON.stringify(options.meta) : '');
+	}
+
+	var transportConsole = new (winston.transports.Console)({
+		level: 'debug', /* { error: 0, warn: 1, info: 2, verbose: 3, debug: 4, silly: 5 } */
+		json: false, /* true : will log out multi-line JSON objects */
+		stringify: false,
+		timestamp: timestamp,
+		formatter: formatter
 	});
+
+	var transportDailyRotateFile = new (winston.transports.DailyRotateFile)({
+		filename: './modules-server/winston/log/log',
+		datePattern: 'yyyy-MM-dd.',
+		prepend: true,
+		level: 'info',
+		json: false,
+		stringify: false,
+		timestamp: timestamp,
+		formatter: formatter
+	});
+
+	var transports = env_mode === "developement" ? [transportConsole] : [transportDailyRotateFile];
+
+	return new (winston.Logger)({ transports: transports });
 }
--- modules-server/winston/logger.winston.test.js
+++ modules-server/winston/logger.winston.test.js
@@ -1,16 +1,19 @@
-var logger = require('./logger.winston')(__filename);
+var logger = require('./logger.winston')(__filename || "Process Name");
 
-logger.debug('application [%s] is starting in [%s] mode.', __filename, "developement");
-logger.info('application [%s] is starting in [%s] mode.', __filename, "developement");
-logger.warn('application [%s] is starting in [%s] mode.', __filename, "developement");
-logger.error('application [%s] is starting in [%s] mode.', __filename, "developement");
+var appName = 'Test-Logger-Winston';
+var env_mode = process.env.ENV || "developement";
 
-logger.log('debug', 'application [%s] is starting in [%s] mode.', __filename, "developement");
-logger.log('info', 'application [%s] is starting in [%s] mode.', __filename, "developement");
-logger.log('warn', 'application [%s] is starting in [%s] mode.', __filename, "developement");
-logger.log('error', 'application [%s] is starting in [%s] mode.', __filename, "developement");
+logger.debug('application [%s] is starting in [%s] mode.');
+logger.info('application [%s] is starting in [%s] mode.', appName, env_mode);
+logger.warn('application [%s] is starting in [%s] mode.', appName, env_mode);
+logger.error('application [%s] is starting in [%s] mode.', appName, env_mode);
+logger.log('debug', 'application [%s] is starting in [%s] mode.', appName, env_mode);
+logger.log('info', 'application [%s] is starting in [%s] mode.', appName, env_mode);
+logger.log('warn', 'application [%s] is starting in [%s] mode.', appName, env_mode);
+logger.log('error', 'application [%s] is starting in [%s] mode.', appName, env_mode);
 
-var obj1 = {"cookie":{"path":"/","_expires":null,"originalMaxAge":null,"httpOnly":true},"messages":[{"type":"message_error","content":"Sorry! invalid credentials."}]};
+var objSmaple = {"cookie":{"path":"/","_expires":null,"originalMaxAge":null,"httpOnly":true},"messages":[{"type":"message_error","content":"Sorry! invalid credentials."}]};
 
-logger.debug(obj1);
-logger.debug('obj1=', obj1);
+logger.debug('objSmaple=' + objSmaple);
+logger.debug('objSmaple=%s', objSmaple);
+logger.debug('objSmaple=%j', objSmaple);

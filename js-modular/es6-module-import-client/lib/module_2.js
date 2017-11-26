e921f533902823c6fcf43f8730d65ecce6d7d1c2
loggar <webnl@DT-Charly.koi.local>
2017-11-27 09:39:12 +1100

    es6 import at client : window obj

---
+++ js-modular/es6-module-import-client/lib/module_2.js
@@ -0,0 +1,8 @@
+export function module_2() {
+    return {
+        f: function () {
+            console.log("module_2.f()");
+        },
+        v: "charly module_2"
+    }
+}
---
+++ js-modular/es6-module-import-client/lib/module_3.js
@@ -0,0 +1,3 @@
+export function module_3(o) {
+	console.log(o);
+}

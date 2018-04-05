b3dce92613b2a7133fd07833a98b2aed7ce92f76
loggar <webnl@DT-Charly.koi.local>
2018-04-05 10:17:41 +1000

    closure

---
+++ practice-js/closure/closure.object.1.js
@@ -0,0 +1,24 @@
+var constructor = function () {
+	var x = 0;
+	return {
+		a: 1,
+		b: "b",
+
+		x: function () {
+			return x;
+		},
+		setX: function (_x) {
+			x = _x;
+		}
+	}
+}
+
+var a = constructor();
+
+var b = constructor();
+
+b.a = 2;
+b.setX(-1);
+
+console.log(a, a.x());
+console.log(b, b.x());

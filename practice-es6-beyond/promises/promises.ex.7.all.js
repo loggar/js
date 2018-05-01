a83362c683b31f593484b6b4ca1a680c29e7d37d
loggar <webnl@DT-Charly.koi.local>
2018-05-01 12:22:47 +1000

    js promises

--- practice-es6-beyond/promises/promises.ex.7.all.js
+++ practice-es6-beyond/promises/promises.ex.7.all.js
@@ -3,9 +3,15 @@ Think about JavaScript loaders: there are times when you trigger multiple async 
 -- that's where Promise.all comes in.  The Promise.all method takes an array of promises and fires one callback once they are all resolved:
 */
 
-Promise.all([promise1, promise2]).then(function(results) {
+Promise.all([promise1, promise2]).then(function (results) {
 	// Both promises resolved
 })
-.catch(function(error) {
-	// One or more promises was rejected
-});
+	.catch(function (error) {
+		// One or more promises was rejected
+	});
+
+
+/*
+with ES6 destructuring sugar:
+*/
+Promise.all([promise1, promise2]).then(function ([result1, result2]) { /*  */ });

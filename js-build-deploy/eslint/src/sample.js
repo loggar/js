537f40b56cda57d5d7044e740b3cfe51a9251bb0
loggar <webnl@DT-Charly.koi.local>
2018-05-10 09:30:04 +1000

    eslint

--- js-build-deploy/eslint/src/sample.js
+++ js-build-deploy/eslint/src/sample.js
@@ -4,3 +4,9 @@ request.open('POST', url, true);
 request.onreadystatechange = readyStateChange.bind({}, d, xhr);
 request.upload.onprogress = onProgress.bind({}, d, xhr);
 request.send(fd);
+
+var i = 0;
+
+if (i == 0) {
+	console.log("i=0");
+}

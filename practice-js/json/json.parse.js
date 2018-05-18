4db30c299506ae2f91776c93d490a0f672d86e19
loggar <webnl@DT-Charly.koi.local>
2018-05-18 15:43:29 +1000

    json.parse

---
+++ practice-js/json/json.parse.js
@@ -0,0 +1,9 @@
+var marks = { physics: 98, maths: 95, chemistry: 91 };
+
+var j1 = JSON.stringify(marks);
+
+console.log(j1);
+
+var o1 = JSON.parse('{"physics":98,"maths":95,"chemistry":91}');
+
+console.log(o1);

97c2b30752e670ae4b6fd413deeb207d74928163
Charly LEE <charly.loggar@gmail.com>
2018-06-03 19:25:20 +1000

    NaN

--- practice-js/types/primative-boolean-false.js
+++ practice-js/types/primative-boolean-false.js
@@ -1,7 +1,37 @@
+/*
 false
 undefined
 null
 0
 -0
 NaN
-"" // the empty string
+'', "" // the empty string
+*/
+
+if(!false) {
+	console.log('!false');
+}
+
+if(!undefined) {
+	console.log('!false');
+}
+
+if(!null) {
+	console.log('!false');
+}
+
+if(!0) {
+	console.log('!false');
+}
+
+if(!(-0)) {
+	console.log('!false');
+}
+
+if(!NaN) {
+	console.log('!false');
+}
+
+if(!'') {
+	console.log('!false');
+}

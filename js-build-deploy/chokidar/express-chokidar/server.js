54e893e354cb6196ede3fe67bec0432d18a89c03
loggar <webnl@DT-Charly.koi.local>
2018-06-04 16:05:00 +1000

    express-chokidar

--- js-build-deploy/chokidar/express-chokidar/server.js
+++ js-build-deploy/chokidar/express-chokidar/server.js
@@ -1,14 +1,22 @@
 var express = require('express')
 var app = express()
 var chokidar = require('chokidar')
-var watcher = chokidar.watch('./app')
 var port = process.env.PORT || 9000
 
-// exclude in production
-/*
-var production = process.env.NODE_ENV === 'production'
-if (!production) {
-	var chokidar = require('chokidar')
+var env_mode = process.env.NODE_ENV || 'developement';
+if (env_mode !== 'production') {
+	var watcher = chokidar.watch('./app')
+	watcher.on('ready', function () {
+		watcher.on('all', function () {
+			console.log("Clearing /dist/ module cache from server")
+			Object.keys(require.cache).forEach(function (id) {
+				if (/[\/\\]app[\/\\]/.test(id)) delete require.cache[id]
+			})
+		})
+	})
+} else {
+	// exclude in production
+	/*
 	var watcher = chokidar.watch('./dist')
 	watcher.on('ready', function () {
 		watcher.on('all', function () {
@@ -18,17 +26,9 @@ if (!production) {
 			})
 		})
 	})
+	*/
 }
-*/
 
-watcher.on('ready', function () {
-	watcher.on('all', function () {
-		console.log("Clearing /dist/ module cache from server")
-		Object.keys(require.cache).forEach(function (id) {
-			if (/[\/\\]app[\/\\]/.test(id)) delete require.cache[id]
-		})
-	})
-})
 app.use(function (req, res, next) {
 	require('./app/index')(req, res, next)
 })

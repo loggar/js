ad2d812fa1ed7bf3f8b705e7dd2b9c6f2d718dfd
loggar <webnl@DT-Charly.koi.local>
2018-06-05 15:50:06 +1000

    hot-reloading-backend

--- js-build-deploy/webpack/hot-reloading-backend-frontend/bin/www.js
+++ js-build-deploy/webpack/hot-reloading-backend-frontend/bin/www.js
@@ -1,6 +1,6 @@
 import http from 'http'
 import app from '../src-server/app'
-const server = http.createServer(app)
+const server = http.createServer(app) // in other to applying hot-reloading, server and app should be defined separately.
 let currentApp = app
 server.listen(3000)
 if (module.hot) {

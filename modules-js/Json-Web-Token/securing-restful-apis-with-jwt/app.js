d35f4d8bcf50574c69a25310dee072d2a61e1463
loggar <webnl@DT-Charly.koi.local>
2018-06-12 10:52:55 +1000

    securing-restful-apis-with-jwt

--- modules-js/Json-Web-Token/securing-restful-apis-with-jwt/app.js
+++ modules-js/Json-Web-Token/securing-restful-apis-with-jwt/app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
-global.__root   = __dirname + '/'; 
+global.__root = __dirname + '/';
 
 app.get('/api', function (req, res) {
-  res.status(200).send('API works.');
+	res.status(200).send('API works.');
 });
 
 var UserController = require(__root + 'user/UserController');

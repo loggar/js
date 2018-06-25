94efc8debc405ad8e29a465fd3374418ee0c6f82
loggar <webnl@DT-Charly.koi.local>
2018-06-25 12:29:50 +1000

    promise-sequentail-functional

--- docs/sample-res/readme.md
+++ docs/sample-res/readme.md
@@ -5,4 +5,8 @@ example response
 ```
 https://loggar.github.io/js/docs/sample-res/sample.1.json
 
+https://loggar.github.io/js/docs/sample-res/charly-loggar-logo.png
+
+https://loggar.github.io/js/docs/sample-res/sample.1.html
+
 ...
---
+++ refs-js/promise/promise-sequentail-functional.js
@@ -0,0 +1,27 @@
+require('isomorphic-fetch');
+
+/*
+ * promiseSerial resolves Promises sequentially.
+ * @example
+ * const urls = [ ... ]
+ * const funcs = urls.map(url => () => $.ajax(url))
+ *
+ * promiseSerial(funcs)
+ *   .then(console.log)
+ *   .catch(console.error)
+ */
+const promiseSerial = funcs =>
+	funcs.reduce((promise, func) =>
+		promise.then(result => func().then(Array.prototype.concat.bind(result))),
+		Promise.resolve([]))
+
+// some url's to resolve
+const urls = ['https://loggar.github.io/js/docs/sample-res/sample.1.json', 'https://loggar.github.io/js/docs/sample-res/sample.2.json', 'https://loggar.github.io/js/docs/sample-res/sample.3.json']
+
+// convert each url to a function that returns a promise
+const funcs = urls.map(url => () => fetch(url))
+
+// execute Promises in serial
+promiseSerial(funcs)
+	.then(console.log.bind(console))
+	.catch(console.error.bind(console))

aafa808123f066dfb9ce145755c23986a68fdd32
loggar <webnl@DT-Charly.koi.local>
2018-07-03 15:00:40 +1000

    event-driven-architecture

---
+++ refs-js/event-driven-architecture/event-emitter.once.js
@@ -0,0 +1,8 @@
+var events = require('events');
+var eventEmitter = new events.EventEmitter();
+eventEmitter.once('data_received', function () {
+	console.log('data received succesfully.');
+});
+
+eventEmitter.emit('data_received'); // triggered
+eventEmitter.emit('data_received'); // not triggered

a07b09426ce99acbb0483af16e73aa22a8360552
Charly LEE <charly.loggar@gmail.com>
2018-07-11 21:22:25 +1000

    headless browser test puppeteer

--- modules-test/client.puppeteer/src-puppeteer/ex2.generate-pdf.js
+++ modules-test/client.puppeteer/src-puppeteer/ex2.generate-pdf.js
@@ -4,7 +4,7 @@ const path = require('path');
 (async () => {
 	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
-	await page.goto('https://github.com/loggar', { waitUntil: 'networkidle' });
+	await page.goto('https://github.com/loggar', { waitUntil: 'networkidle2' });
 	await page.pdf({
 		path: path.join(__dirname, 'screenpdf', 'github_1.pdf'),
 		format: 'A4'

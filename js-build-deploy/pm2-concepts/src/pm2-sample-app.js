84c309094abc219d2001ddfb0d948cb6b1992d7c
loggar <webnl@DT-Charly.koi.local>
2018-07-30 12:38:22 +1000

    pm2-concepts

--- js-build-deploy/pm2-concepts/src/pm2-sample-app.js
+++ js-build-deploy/pm2-concepts/src/pm2-sample-app.js
@@ -1,10 +1,7 @@
 var env_mode = process.env.NODE_ENV || 'development';
-var path = require('path');
-var log_dir = path.resolve(__dirname, '../../../../');
-var logger = require('../../../../modules-lib/winston/logger.winston')(__filename || 'Process Name', 'debug', 1, log_dir + '/_log/pm2-sample-app.log');
 var schedule = require('node-schedule');
 
-logger.info('Start Application : %s in %s environment', __filename, env_mode);
+console.log('Start Application : %s in %s environment', __filename, env_mode);
 var s1 = schedule.scheduleJob('10 * * * * *', function () {
-	logger.info('%s %s', new Date(), '10 * * * * *');
+	console.log('%s %s', new Date(), '10 * * * * *');
 });

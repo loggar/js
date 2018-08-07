badab5e148c9953105a7ff602a224aa2e7d9e6b7
loggar <webnl@DT-Charly.koi.local>
2018-08-07 11:37:48 +1000

    js types

---
+++ js-core/types/boolean.js
@@ -0,0 +1,12 @@
+function bool_01() {
+	var b1 = false;
+	var b2 = true;
+
+	console.log(typeof b1);
+	console.log(b1);
+	console.log(typeof b2);
+	console.log(b2);
+}
+
+/* test code */
+bool_01();

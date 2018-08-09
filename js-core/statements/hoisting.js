3e6c855b52fabd28b6422a40f9c9078e5e3fa620
loggar <webnl@DT-Charly.koi.local>
2018-08-09 10:20:12 +1000

    refactor(hoisting)

--- js-core/statements/hoisting.js
+++ js-core/statements/hoisting.js
@@ -9,3 +9,18 @@ function doSomething(a) {
 1. First scan the program, collect all the variable and function declarations and assign memory spaces for it.
 2. Run the program now by filling variable values assigned any, if not, fill undefined
 */
+
+/*
+var : hoisted in the scope it belongs to
+let : not hoisted
+*/
+
+/* before hoisting
+test(a);
+var a = 3;
+*/
+/* after hoisting
+var a;
+test(a);   // a : undefined ( it has been declared, but has no defined value yet.)
+a = 3;
+*/

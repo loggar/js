00929ff480bb56ef86f2e8d6326df54dfc5f911d
loggar <webnl@DT-Charly.koi.local>
2018-08-29 11:46:49 +1000

    feat (json) arr to json

---
+++ js-core/json/json.arr-to-json.js
@@ -0,0 +1,5 @@
+var arr = [{ name: "John", age: 31, city: "New York" }, { name: "Jack", age: 32, city: "Sydney" }, { name: "Mike", age: 33, city: "Town" }];
+
+var j = JSON.stringify(arr);
+
+console.log(j);

997b583a18339e14fed9c537ad9577e84cee6239
loggar <webnl@DT-Charly.koi.local>
2018-11-13 14:01:00 +1100

    feat (string) substr

--- js-core/strings/string.substring.js
+++ js-core/strings/string.substring.js
@@ -5,3 +5,8 @@ console.log(str.substring(7));
 
 var str = "ACC705LA0218";
 console.log(str.substring(0, 8))
+
+
+var integerVal = '-150';
+var absIntVal = integerVal.substr(1, integerVal.length - 1);
+console.log(absIntVal);

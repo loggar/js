e101770a709b24a4413dbd4ee7d7d8d07fb2c000
loggar <webnl@DT-Charly.koi.local>
2018-12-17 12:16:03 +1100

    feat (math) methods

---
+++ js-core/numbers/math.js
@@ -0,0 +1,57 @@
+Math.abs(2.5) //2.5
+Math.abs(-2.5) //2.5
+
+Math.acos(0.8) //0.6435011087932843
+
+Math.asin(0.8) //0.9272952180016123
+
+Math.atan(30) //1.5374753309166493
+
+Math.atan2(30, 20) //0.982793723247329
+
+Math.ceil(2.5) //3
+Math.ceil(2) //2
+Math.ceil(2.1) //3
+Math.ceil(2.99999) //3
+
+Math.cos(0) //1
+Math.cos(Math.PI) //-1
+
+Math.exp(1) //2.718281828459045
+Math.exp(2) //7.38905609893065
+Math.exp(5) //148.4131591025766
+
+Math.floor(2.5) //2
+Math.floor(2) //2
+Math.floor(2.1) //2
+Math.floor(2.99999) //2
+
+Math.log(10) //2.302585092994046
+Math.log(Math.E) //1
+
+Math.max(1,2,3,4,5) //5
+Math.max(1) //1
+
+Math.min(1,2,3,4,5) //1
+Math.min(1) //1
+
+Math.pow(1, 2) //1
+Math.pow(2, 1) //2
+Math.pow(2, 2) //4
+Math.pow(2, 4) //16
+
+Math.random() //0.9318168241227056
+Math.random() //0.35268950194094395
+
+Math.round(1.2) //1
+Math.round(1.6) //2
+
+Math.sin(0) //0
+Math.sin(Math.PI) //1.2246467991473532e-16)
+
+Math.sqrt(4) //2
+Math.sqrt(16) //4
+Math.sqrt(5) //2.23606797749979
+
+Math.tan(0) //0
+Math.tan(Math.PI) //-1.2246467991473532e-16

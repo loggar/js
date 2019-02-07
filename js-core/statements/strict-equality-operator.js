ef8be0979769f5aac5a65b3d95a1837cb3e7acba
loggar <webnl@DT-Charly.koi.local>
2019-02-08 10:39:24 +1100

    feat (strict equality operator)

---
+++ js-core/statements/strict-equality-operator.js
@@ -0,0 +1,26 @@
+console.log("'' == '0' ", '' == '0');
+console.log("'' === '0' ", '' === '0');
+
+console.log("0 == ''", 0 == '');
+console.log("0 === ''", 0 === '');
+
+console.log("0 == '0'", 0 == '0');
+console.log("0 === '0'", 0 === '0');
+
+console.log("false == 'false'", false == 'false');
+console.log("false === 'false'", false === 'false');
+
+console.log("false == '0' ", false == '0');
+console.log("false === '0' ", false === '0');
+
+console.log('false == undefined', false == undefined);
+console.log('false === undefined', false === undefined);
+
+console.log('false == null', false == null);
+console.log('false == null', false === null);
+
+console.log('null == undefined', null == undefined);
+console.log('null === undefined', null === undefined);
+
+console.log("' \\t\\r\\n ' == 0", ' \t\r\n ' == 0);
+console.log("' \\t\\r\\n ' === 0", ' \t\r\n ' === 0);

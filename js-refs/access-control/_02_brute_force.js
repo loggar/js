e4411865358514d8ec634bb369ee22a486d45c47
loggar <webnl@DT-Charly.koi.local>
2019-03-05 14:56:22 +1100

    refactor (test)

--- js-refs/access-control/_02_brute_force.js
+++
@@ -1,7 +0,0 @@
-crypto.pbkdf2(password, salt,
-  iterations, keyLength, function(err, hash) {
-    if (err) {
-      return callback(err);
-    }
-    callback(null, new Buffer(hash).toString('base64'));
-  });

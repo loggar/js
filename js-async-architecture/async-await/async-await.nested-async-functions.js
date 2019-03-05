e476d0fc8e3cae2f7c4bcfcd73f1b3d600e2c7ca
loggar <webnl@DT-Charly.koi.local>
2019-03-06 10:37:11 +1100

    feat (es6) async-await

---
+++ js-async-architecture/async-await/async-await.nested-async-functions.js
@@ -0,0 +1,18 @@
+const promiseToDoSomething = () => {
+  return new Promise(resolve => {
+    setTimeout(() => resolve('I did something'), 10000);
+  });
+};
+const watchOverSomeoneDoingSomething = async () => {
+  const something = await promiseToDoSomething();
+  return something + ' and I watched';
+};
+const watchOverSomeoneWatchingSomeoneDoingSomething = async () => {
+  const something = await watchOverSomeoneDoingSomething();
+  return something + ' and I watched as well';
+};
+watchOverSomeoneWatchingSomeoneDoingSomething().then(res => {
+  console.log(res);
+});
+
+// I did something and I watched and I watched as well

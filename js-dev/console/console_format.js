7a9abc864ad2e1e1449ae20760307412259b348a
loggar <webnl@DT-Charly.koi.local>
2019-04-09 15:00:56 +1000

    feat (console) format

--- js-dev/console/console_format.js
+++ js-dev/console/console_format.js
@@ -1,33 +1,40 @@
-console.log("my %s has %d years", 'dog', 15);
-console.log("my %s has %i years", 'dog', 15);
+console.log('my %s has %d years', 'dog', 15);
+console.log('my %s has %i years', 'cat', 15);
 
-console.log("floating point number example : %f", Math.PI);
+console.log('floating point number example : %f', Math.PI);
 
 const obj = {
-	facebook: false,
-	linkedin: true,
-	flickr: false,
-	instagram: false,
-	stackoverflow: true,
-	github: true,
-	mozillar: true,
-	hackernoon: true,
-	freecodecamp: true
-}
+  facebook: false,
+  linkedin: true,
+  flickr: false,
+  instagram: false,
+  stackoverflow: true,
+  github: true,
+  mozillar: true,
+  hackernoon: true,
+  freecodecamp: true,
+  nested: {
+    name: 'me',
+    addr: 'addr'
+  }
+};
 
-console.log("site interesting : %O", obj);
-console.log("site interesting : %j", obj);
+console.log(obj);
+console.dir(obj);
+console.log(JSON.stringify(obj, ' '));
+console.log('site interesting : %O', obj);
+console.log('site interesting : %j', obj);
 
 if (typeof window !== 'undefined') {
-	console.log("%o, %O", document.body, document.body);
+  console.log('%o, %O', document.body, document.body);
 
-	console.log(
-		'%c My %s has %d years',
-		'color: yellow; background:black; font-size: 16pt',
-		'cat',
-		2
-	);
+  console.log(
+    '%c My %s has %d years',
+    'color: yellow; background:black; font-size: 16pt',
+    'cat',
+    2
+  );
 
-	console.log([1, 2]);
-	console.dir([1, 2]);
+  console.log([1, 2]);
+  console.dir([1, 2]);
 }

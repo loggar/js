a6440694f2e0db483ea91413cace89e609ba52dd
loggar <webnl@DT-Charly.koi.local>
2019-07-02 17:38:28 +1000

    feat (variable) assignment

---
+++ js-es/call-by-value-ref/variable-assignment-ref.js
@@ -0,0 +1,7 @@
+let var1 = { name: "Jim" };
+let var2 = var1;
+var2.name = "John";
+console.log(var1);
+// { name: 'John' }
+console.log(var2);
+// { name: 'John' }
---
+++ js-es/call-by-value-ref/variable-assignment-value.js
@@ -0,0 +1,7 @@
+let var1 = "My string";
+let var2 = var1;
+var2 = "My new string";
+console.log(var1);
+// 'My string'
+console.log(var2);
+// 'My new string'

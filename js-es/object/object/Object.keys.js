75e03ad21bc86044d8075f55efb6c37e4d87ea51
Charly LEE <charly.loggar@gmail.com>
2019-11-27 13:39:42 +1100

    feat (object) keys

--- js-es/object/object/Object.keys.js
+++ js-es/object/object/Object.keys.js
@@ -2,8 +2,17 @@ var marks = { physics: 98, maths: 95, chemistry: 91 };
 
 var highScore = 0;
 for (i of Object.keys(marks)) {
-	if (marks[i] > highScore)
-		highScore = marks[i];
+  if (marks[i] > highScore) highScore = marks[i];
 }
 
 console.log(highScore);
+
+var params = { physics: 98, maths: 95, chemistry: 91 };
+
+for (var key in params) {
+  console.log(key, params[key]);
+}
+
+for (key of Object.keys(params)) {
+  console.log(key, params[key]);
+}

481ee81ef424a53a53891cf2ce4ea4e35cfe478f
Charly LEE <charly.loggar@gmail.com>
2019-12-09 16:20:31 +1100

    feat (string)

---
+++ js-es/strings/strings/string.substring.3.js
@@ -0,0 +1,8 @@
+var str = "1999 Other Studies in Creative Arts and Writing";
+
+var str1 = str.substr(0, 4);
+console.log(str1);
+console.log(str1.length);
+
+var str2 = str.substring(5);
+console.log(str2);

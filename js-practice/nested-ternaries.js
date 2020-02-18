bc87063283c833089d7f6205738711dd92599290
Charly LEE <charly.loggar@gmail.com>
2020-02-18 17:33:01 +1100

    feat (better practices)

---
+++ js-practice/nested-ternaries.js
@@ -0,0 +1,12 @@
+let result = null;
+if (conditionA) {
+  if (conditionB) {
+    result = "A & B";
+  } else {
+    result = "A";
+  }
+} else {
+  result = "Not A";
+}
+
+const result = !conditionA ? "Not A" : conditionB ? "A & B" : "A";

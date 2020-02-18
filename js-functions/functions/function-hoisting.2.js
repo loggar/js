870062f38a6ada54dbbe1cba5970480911f8e36b
Charly LEE <charly.loggar@gmail.com>
2020-02-18 17:18:01 +1100

    refactor (dirs)

---
+++ js-functions/functions/function-hoisting.2.js
@@ -0,0 +1,5 @@
+console.log(square); // square is hoisted with an initial value undefined.
+console.log(square(5)); // TypeError: square is not a function
+var square = function (n) {
+	return n * n;
+}

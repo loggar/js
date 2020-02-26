d756750d3ff4080d9edfc764d8146dcdc3355767
Charly LEE <charly.loggar@gmail.com>
2020-02-27 10:32:40 +1100

    fix (array) duplicates

--- js-arrays/arrays.remove-duplicates/array.remove-duplicates.2.js
+++ js-arrays/arrays.remove-duplicates/array.remove-duplicates.2.js
@@ -1,8 +1,4 @@
 // ES5 Implementation
-var array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
-
-uniqueArray(array); // [1, 2, 3, 5, 9, 8]
-
 function uniqueArray(array) {
   var hashmap = {};
   var unique = [];
@@ -17,3 +13,7 @@ function uniqueArray(array) {
 
   return unique;
 }
+
+var array = [1, 2, 3, 5, 1, 5, 9, 1, 2, 8];
+
+console.log(uniqueArray(array)); // [1, 2, 3, 5, 9, 8]

260cfc9dc217636d1178a847f153bec3f87065b1
Charly LEE <charly.loggar@gmail.com>
2020-03-10 10:35:18 +1100

    feat (console) assert

--- js-dev/console/console_assert.js
+++ js-dev/console/console_assert.js
@@ -1,5 +1,13 @@
-var clt = function (a, b) {
-	console.assert(a < b, { message: '!(a < b)', a: a, b: b })
-}
+var clt = function(a, b) {
+  console.assert(a < b, { message: "!(a < b)", a: a, b: b });
+};
 
-clt(2,1)
+clt(2, 1);
+
+const sum = (n1, n2) => {
+  console.assert(n1 + n2 === 10, "Not 10");
+};
+
+sum(3, 2); // Assertion failed: Not 10
+sum(5, 5);
+sum(10, 0);

30ad6e34050f1f72e25ac42bba3d520bfa892dcd
Charly LEE <charly.loggar@gmail.com>
2020-04-28 23:11:49 +1000

    feat (modular) es5 function closure

--- js-es/object/object/Object.prototype.hasOwnProperty.js
+++ js-es/object/object/Object.prototype.hasOwnProperty.js
@@ -1,4 +1,5 @@
-var marks = { physics: 98, maths: 95, chemistry: 91 };
+var marks = { physics: 98, maths: 95, chemistry: 91, falsyValue: false };
 
-marks.hasOwnProperty("physics"); // returns true
-marks.hasOwnProperty("greek"); // returns false
+console.log(marks.hasOwnProperty("physics")); // returns true
+console.log(marks.hasOwnProperty("greek")); // returns false
+console.log(marks.hasOwnProperty("falsyValue")); // returns true

74afcaf81e9658e5e94fdbf78f3311ad30ba03e7
Charly LEE <charly.loggar@gmail.com>
2020-05-25 12:27:54 +1000

    feat (map)

--- js-es/map/map.forEach.js
+++ js-es/map/map.forEach.js
@@ -1,8 +1,9 @@
 var myMap = new Map();
-myMap.set(0, 'zero');
-myMap.set(1, 'one');
+myMap.set(0, "zero");
+myMap.set(1, "one");
 
-myMap.forEach(function (value, key) {
-	console.log(key + ' = ' + value);
+myMap.forEach((value, key) => {
+  console.log(key, value);
 });
-// Will show 2 logs; first with "0 = zero" and second with "1 = one"
+// 0 zero
+// 1 one

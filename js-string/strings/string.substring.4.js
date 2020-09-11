74baf589e49be4d0a45080a28b653b6696ebf05e
Charly LEE <charly.loggar@gmail.com>
2020-09-11 14:31:11 +1000

    feat (string) substring

--- js-string/strings/string.substring.4.js
+++ js-string/strings/string.substring.4.js
@@ -27,11 +27,17 @@ var getId2 = function (v) {
 };
 
 var getId3 = function (v) {
-  i_s = v.indexOf("] ");
-  i_e = v.indexOf(" (");
-  if (i_s < 0 || i_e < 0) {
-    return "";
+  i_s = v.indexOf("] ") || 0;
+  i_e = v.indexOf(" (") || v.length;
+
+  if (i_s <= 0) {
+    i_s = -2;
   }
+
+  if (i_e < 0) {
+    i_e = v.length;
+  }
+
   var r = v.substring(i_s + 2, i_e);
   if (!r) {
     return "";
@@ -58,8 +64,12 @@ var parseInvoiceId = function (invoiceId) {
   }
 };
 
-console.log("1", parseInvoiceId(""));
-console.log("2", parseInvoiceId("[ABC-2020-000004]  ()"));
-console.log("3", parseInvoiceId("[ABC-2020-000004] A4220095135 ()"));
-console.log("4", parseInvoiceId("[ABC-2020-000004]  (12000068)"));
-console.log("5", parseInvoiceId("[ABC-2020-000004] A4220095135 (12000068)"));
+// console.log("1", parseInvoiceId(""));
+// console.log("2", parseInvoiceId("[ABC-2020-000004]  ()"));
+// console.log("3", parseInvoiceId("[ABC-2020-000004] A4220095135 ()"));
+// console.log("4", parseInvoiceId("[ABC-2020-000004]  (12000068)"));
+// console.log("5", parseInvoiceId("[ABC-2020-000004] A4220095135 (12000068)"));
+// console.log("5", parseInvoiceId("ABC-2020-000004"));
+// console.log("6", parseInvoiceId("A4220095135 (12000068)"));
+console.log("7", parseInvoiceId("[ABC-2020-000004] A4220095135"));
+console.log("8", parseInvoiceId(" A4220095135"));

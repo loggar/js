fda07c882867eda74a553bcff62e24a3b51255c3
Charly LEE <charly.loggar@gmail.com>
2020-10-15 17:41:12 +1100

    feat (json) format

--- js-es/json/json.format.js
+++ js-es/json/json.format.js
@@ -1,7 +1,15 @@
-console.log(JSON.stringify({ alpha: 'A', beta: 'B' }, null, '\t'));
+console.log(JSON.stringify({ alpha: "A", beta: "B" }, null, "\t"));
 /*
 {
 	"alpha": "A",
 	"beta": "B"
+}
+*/
+
+console.log(JSON.stringify({ name: "John", Age: 23 }, null, "\t"));
+/*
+{
+	"name": "John",
+	"Age": 23
 }
 */

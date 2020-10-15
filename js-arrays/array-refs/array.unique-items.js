bd6fbe743c4bf98875750cb013e538d1c80bdafd
Charly LEE <charly.loggar@gmail.com>
2020-10-15 17:47:37 +1100

    feat (array) unique

---
+++ js-arrays/array-refs/array.unique-items.js
@@ -0,0 +1,18 @@
+let uniqueArray = [
+  ...new Set([
+    1,
+    2,
+    3,
+    3,
+    3,
+    "school",
+    "school",
+    "ball",
+    false,
+    false,
+    true,
+    true,
+  ]),
+];
+
+console.log(uniqueArray);

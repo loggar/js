30cccad9431b30fc1732f4b39421869bc5a19555
Charly LEE <charly.loggar@gmail.com>
2020-10-15 17:35:01 +1100

    feat (boolean)

--- js-es/booleans/convert-to-boolean.js
+++ js-es/booleans/convert-to-boolean.js
@@ -3,3 +3,11 @@ const isFalse = !1;
 const alsoFalse = !!0;
 console.log(true); // Result: true
 console.log(typeof true); // Result: "boolean"
+
+console.log(!!23); // true
+console.log(!!0); // false
+console.log(!!""); // false
+console.log(!!"a"); // true
+console.log(!!{}); // true
+console.log(!!null); // false
+console.log(!!undefined); // false

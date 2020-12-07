2b346d80580e5565afd58f0d919a32f37343d5a0
Charly LEE <charly.loggar@gmail.com>
2020-12-07 17:49:54 +1100

    feat (await) fetch with delay in a loop

---
+++ js-async-promise-await/async-await.fetch/async-await.fetch-seq-delay-loop.js
@@ -0,0 +1,39 @@
+const taskDelayDecorator = function (resolve, ms) {
+  return new Promise(function (resolve, reject) {
+    return setTimeout(function () {
+      resolve();
+    }, ms);
+  });
+};
+
+const taskPromise = function (reqIdx, id, accumulator) {
+  return new Promise((resolve, reject) => {
+    fetch("/url", {
+      method: "POST",
+      body: JSON.stringify({
+        id: id,
+      }),
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        koiLogger.info(reqIdx, "taskPromise", data);
+        accumulator.push(data);
+      });
+  });
+};
+
+const fn_req_delete_invoice = async function (arr) {
+  var rarr = [];
+  for (var i = 0; i < arr.length; i++) {
+    await taskDelayDecorator(
+      taskInvoiceDelete(i, invoices[i].invoiceNo, rarr),
+      500
+    );
+  }
+};

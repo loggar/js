c4ba0f6dcd293ebf22a2b8f78f0f902f0e4a7b33
Charly LEE <charly.loggar@gmail.com>
2021-03-23 17:45:35 +1100

    feat (object) is

--- js-es.class-object/object-es6-beyond/object.is.js
+++ js-es.class-object/object-es6-beyond/object.is.js
@@ -1,7 +1,16 @@
-var x = NaN, y = 0, z = -0;
+var x = NaN,
+  y = 0,
+  z = -0;
 
-x === x;                            // false
-y === z;                            // true
+x === x; // false
+y === z; // true
 
-Object.is(x, x);                    // true
-Object.is(y, z);                    // false
+Object.is(x, x); // true
+Object.is(y, z); // false
+
+Object.is(0, 0); // true
+Object.is(null, null); // true
+Object.is(undefined, undefined); // true
+Object.is(true, 1); // false
+Object.is(+0, -0); // false
+Object.is(NaN, NaN); // true
